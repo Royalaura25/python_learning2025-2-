1d7e538dce07124408dd6bac784c2434307d9418
Eniola Okusaga <eniolaokusaga91@gmail.com>
2025-07-17 11:57:53 +0100

    uploading all assignments

---
+++ update.py
@@ -0,0 +1,10 @@
+score=int(input('Enter total score: '))
+print('This student got a grade A' if 80<=score>=100 else 'inavlid score')
+print( 'This student got a grade B' if score>=60 and score<=79 else 'inavlid score' )
+print ('This student got a grade C' if score>=50 and score<=59 else 'inavlid score')
+print ('This student got a grade D' if score>=40 and score<=49 else 'inavlid score' )
+print ('This student got a grade E' if score>=25 and score<=39  else 'inavlid score')
+print ('This student got a grade F' if score>=0 and score<=24 else 'inavlid score')
+    
+    
+
